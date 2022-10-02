73132072565b1d192e494b3bc8386c39f594a3f7
pltnw <k.pltnw@gmail.com>
2022-10-02 23:38:04 +0200

    Setting alerts

--- LogInApp/ViewController.swift
+++ LogInApp/ViewController.swift
@@ -14,17 +14,40 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let welcomeViewController = segue.destination as? WelcomeViewController else { return }
         welcomeViewController.userName = userNameTF.text
-        
     }
+    
     @IBAction func unwind(for segue: UIStoryboardSegue) {
         userNameTF.text = ""
         passwordTF.text = ""
     }
+ 
+    @IBAction func logInButtonDidTapped() {
+        guard let userName = userNameTF.text, let password = passwordTF.text else { return }
+        if userName != "User" || password != "Password" {
+            showAlert(withTitle: "Invalid login or password", andMessage: "Please, enter correct login or password")
+        }
+    }
+    
+    @IBAction func forgotNameDidTapped() {
+        showAlert(withTitle: "Oops!", andMessage: "Your name is User 😊")
+    }
+    
+    @IBAction func forgotPasswordDidTapped() {
+        showAlert(withTitle: "Oops!", andMessage: "Your password is Password 😊")
+    }
+    
 }
 
+extension ViewController {
+    private func showAlert(withTitle title: String, andMessage message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
